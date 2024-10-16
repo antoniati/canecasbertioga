0a1d0d695fc89e8c3db9f9fc7ae23744656daeb8
antoniati <antoniati.felipe@gmail.com>
2024-10-16 12:36:14 -0300

    fix/crypto

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
                 },
           ],
     },
+    webpack: (config) => {            
+      config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+      return config;
+  },
 };
 
 export default nextConfig;
--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
