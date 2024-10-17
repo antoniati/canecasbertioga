6bbf13c713298889cce138a42f9d9c124d8e4fa0
antoniati <antoniati.felipe@gmail.com>
2024-10-17 07:48:43 -0300

    fix/pusher

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 import path from 'path';
-import { fileURLToPath } from 'url'; // Adiciona para lidar com o caminho correto
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 
@@ -17,12 +13,10 @@ const nextConfig = {
                 },
           ],
     },
-    webpack: (config) => {
-        config.resolve.fallback = {
-            crypto: require.resolve('crypto-browserify'), // Polyfill do 'crypto' para o browser
-        };
-        return config;
-    },
+    webpack: (config) => {            
+      config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+      return config;
+  },
 };
 
 export default nextConfig;
